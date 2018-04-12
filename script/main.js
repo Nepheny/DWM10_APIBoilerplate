21f8813cc002af185a9271df051a895789717732
admin <admin@iMac3.local>
2018-04-12 12:11:08 +0200

    Initialization of project

---
+++ script/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    //All the JS and JQuery code will de written inside this anonymous function
+    debugger;
+
+    //Allow to hide and show every section of our application
+    function navigateView (sectionToShow) {
+
+    }
+});
